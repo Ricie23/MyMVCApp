9f853f72954b16c1263b0cea8ea03b7035eaafcb
Ricie23 <ricierice23@gmail.com>
2020-01-24 10:40:55 -0500

    complete

---
+++ MyMVCApp/Controllers/GamesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMVCApp.Controllers
+{
+    public class GamesController : Controller
+    {
+        // GET: Games
+        public ActionResult Index()
+        {
+            return RedirectToAction("ShowallGames", "Games");
+        }
+        public ActionResult ShowAllGames()
+        {
+                  return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddNewGame(string name)
+        {
+            ViewBag.Message = "You added " + name + " to your games.";
+            return View("ConfirmGame");
+        }
+        public ActionResult AddNewGame()
+        {
+            return View("NewGameForm");
+        }
+
+    }
+}
---
+++ MyMVCApp/Models/GameModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyMVCApp.Models
+{
+    public class GameModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public int Hours { get; set; }
+        public bool IsBeaten { get; set; }
+
+    }
+}
